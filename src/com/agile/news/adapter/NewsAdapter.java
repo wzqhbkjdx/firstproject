9217a15afd195cecc9ecf9a1e03081cf2ecc9e28
wzq <823034583@qq.com>
2016-01-06 19:46:51 +0800

    add NewsAdapter

---
+++ src/com/agile/news/adapter/NewsAdapter.java
@@ -0,0 +1,6 @@
+package com.agile.news.adapter;
+
+public class NewsAdapter {
+	
+
+}
--- src/com/agile/news/home/ItemNewsPage.java
+++ src/com/agile/news/home/ItemNewsPage.java
@@ -1,13 +1,21 @@
 package com.agile.news.home;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import com.agile.news.R;
+import com.agile.news.adapter.NewsAdapter;
 import com.agile.news.base.BasePage;
+import com.agile.news.bean.NewsListBean;
+import com.agile.news.bean.NewsListBean.News;
+import com.agile.news.bean.NewsListBean.TopNews;
 import com.agile.news.pullrefreshview.PullToRefreshListView;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ItemNewsPage extends BasePage{
@@ -15,6 +23,22 @@ public class ItemNewsPage extends BasePage{
 	private PullToRefreshListView ptrLv;
 	@ViewInject(R.id.top_news_title)
 	private TextView topNewsTitle;
+	@ViewInject(R.id.top_news_viewpager)
+	private LinearLayout mViewPagerLay;
+	@ViewInject(R.id.dots_ll)
+	private LinearLayout dotLl;
+	private View topNewsView;
+	private String url;
+	private String moreUrl;
+	private ArrayList<News> news = new ArrayList<NewsListBean.News>();
+	private ArrayList<TopNews> topNews;
+	private NewsAdapter adapter;
+	private ArrayList<View> dotList;
+	private ArrayList<String> titleList, urlList;
+	private HashSet<String> readSet = new HashSet<String>();
+	private String  hasReadIds;
+	private RollViewPager mViewPager;
+	public boolean isLoadSuccess;
 
 	public ItemNewsPage(Context context) {
 		super(context);
